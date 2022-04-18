af001c770c9a760bbb5a6108311e463baafe3ddf
dayoungee <dbsek21@naver.com>
2022-04-18 21:01:04 +0900

    리렌더링 최적화하기

--- prepare/front/components/LoginForm.js
+++ prepare/front/components/LoginForm.js
@@ -1,6 +1,14 @@
-import React, {useCallback, useState} from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { Form, Input, Button } from 'antd';
 import Link from 'next/link'
+import styled from 'styled-components';
+
+
+// 리렌더링 조심! 방법1) 스타일 컴포넌트 사용
+// const ButtonWrapper = styled.div`
+//   margin-Top: 10px;
+// `;
+
 
 const LoginForm = () => {
   const [id, setId] = useState('');
@@ -14,6 +22,9 @@ const LoginForm = () => {
     setPassword(e.target.vlaue);
   }, []);
 
+  // 리렌더링 조심 ! 방법2) useMemo 사용. useMemo는 값을 캐싱, useCallback은 함수를 캐싱
+  const style = useMemo(() => ({ marginTop: 10 }), []);
+
   return (
     <Form>
       <div>
@@ -26,10 +37,13 @@ const LoginForm = () => {
         <br />
         <Input name="user-password" value={password} onChange={onChangePassword} required />
       </div>
-      <div>
-        <Button type = "primary" htmlType = "submit" loading = {false}>로그인</Button>
+      {/* <ButtonWrapper> */}
+      <div style={style}>
+
+        <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
         <Link href="/signup"><a><Button>회원가입</Button></a></Link>
       </div>
+      {/* </ButtonWrapper> */}
     </Form>
   );
 }
