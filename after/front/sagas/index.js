05d89e362911f43507a788254e1a43d1f11383e7
dayoungee <dbsek21@naver.com>
2022-04-23 23:15:51 +0900

    사가

--- after/front/sagas/index.js
+++ after/front/sagas/index.js
@@ -1,4 +1,22 @@
-export default function rootSaga(){
+import { all, fork, take, call, put } from 'redux'
+
+function* watchLogin(){
+    yield take('LOG_IN');
+}
+
+function* watchLoout(){
+    yield take('LOG_OUT');
+}
+
+function* watchAddPost(){
+    yield take('ADD_POST');
+}
 
 
+export default function* rootSaga(){
+    yield all([
+        fork(watchLogin),
+        fork(watchLoout),
+        fork(watchAddPost),
+    ]);
 }
