4865001e6062bea1ae9e2128c77e8faf02f356e2
YunDayoung <dyyoon@naver.com>
2022-04-29 14:27:05 +0900

    로그인까지

--- prepare/front/pages/signup.js
+++ prepare/front/pages/signup.js
@@ -12,8 +12,14 @@ const Signup = () => {
   const { signUpLoading, signUpDone, signUpError, me } = useSelector((state) => state.user);
 
   useEffect(() => {
+    if (me && me.id) {
+      Router.replace('/');
+    }
+  }, [me && me.id]);
+
+  useEffect(() => {
     if (signUpDone) {
-      Router.push('/');
+      Router.replace('/');
     }
   }, [signUpDone]);
 
